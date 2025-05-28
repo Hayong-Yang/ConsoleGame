3e623bbd8104cd6223656e6754c13f6dfc0320bc
Hayong Yang <ssk04022@gmail.com>
2025-05-28 18:31:43 +0900

    전체 구성

--- src/skills/Skill.java
+++ src/skills/Skill.java
@@ -1,10 +1,41 @@
 package skills;
 
-import gameCharacter.Character;
-import game.Grid;
+import characters.Character;
 
-public interface Skill {
-    void execute(Character user, Character target, Grid grid);
-    int getMpCost();
-    String getName();
-}
+public abstract class Skill {
+    private String name;
+    private int damage;
+    private int requiredMp;
+
+    public Skill(String name, int damage, int requiredMp) {
+        this.name = name;
+        this.damage = damage;
+        this.requiredMp = requiredMp;
+    }
+
+    public abstract Character doSkill(Character target);
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public int getRequiredMp() {
+        return requiredMp;
+    }
+
+    public void setRequiredMp(int requiredMp) {
+        this.requiredMp = requiredMp;
+    }
+} // class
