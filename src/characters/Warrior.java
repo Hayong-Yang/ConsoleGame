54f2d5b7b9bedf2fe23f9f713f1ccab03267fe62
고현서 <hyeonseogo01@gmail.com>
2025-05-28 23:58:09 +0900

    feat(skill): 스킬 추가

--- src/characters/Warrior.java
+++ src/characters/Warrior.java
@@ -1,82 +1,20 @@
 package characters;
 
 import skills.Attack;
-import skills.Skill;
 
-public class Warrior extends Champion implements Attack {
-
-    private Skill equippedSkill;
-
-    public Warrior(String name) {
-        super(220,    35,    28,    10,       12,      0,  name);
+public class Warrior extends Champion
+{
+    public Warrior(int maxHp, int maxMp, int power, int defence, int critical, int exp, String name)
+    {
+        super(maxHp, maxMp, power, defence, critical, exp, name);
     }
 
     @Override
-    public void speak() {
-        System.out.println(getName() + " : \"명예와 강철을 위해, 나는 오늘도 싸운다!\"");
-    }
+    public void speak() { System.out.println("전사 " + this.getName() + "가 전장에 나선다!"); }
 
     @Override
-    public Champion attack(Champion target) {
-        if (!isAlive()) {
-            System.out.println(getName() + "는(은) 쓰러져 있어 공격할 수 없습니다!");
-            return target;
-        }
+    public void injured() { System.out.println("크윽... 아직 끝나지 않았다! 나는 전사 " + this.getName() + "다!"); }
 
-        int dmg = Math.max(0, getPower() - target.getDefence());
+    public Attack defaultAttack = (target) -> { target.setHp(target.getHp() - this.getPower()); return target; };
 
-        if (isCritical()) {
-            System.out.println("★ CRITICAL HIT! ★");
-            dmg *= 2;
-        }
-
-        int remainingHp = Math.max(0, target.getHp() - dmg);
-        target.setHp(remainingHp);
-
-        System.out.printf("%s가(이) %s에게 %d의 피해를 주었습니다. (남은 HP: %d)\n",
-                getName(), target.getName(), dmg, remainingHp);
-
-        return target;
-    }
-
-    public Champion useSkill(Skill skill, Champion target) {
-        if (getMp() < skill.getRequiredMp()) {
-            System.out.println("MP가 부족합니다!");
-            return target;
-        }
-        setMp(getMp() - skill.getRequiredMp());
-        return skill.doSkill(target);
-    }
-
-    public void gainExp(int amount) {
-        setExp(getExp() + amount);
-        while (getExp() >= getMaxExp()) {
-            levelUp();
-        }
-    }
-
-    private void levelUp() {
-        setExp(getExp() - getMaxExp());
-        setLevel(getLevel() + 1);
-        setMaxExp(getMaxExp() + 50);
-
-        setMaxHp(getMaxHp() + 25);
-        setMaxMp(getMaxMp() + 6);
-        setPower(getPower() + 4);
-        setDefence(getDefence() + 3);
-
-        setHp(getMaxHp());
-        setMp(getMaxMp());
-
-        System.out.printf("=== LEVEL UP! %s는(은) Lv.%d이(가) 되었습니다! ===\n",
-                getName(), getLevel());
-    }
-
-    public Skill getEquippedSkill() {
-        return equippedSkill;
-    }
-
-    public void setEquippedSkill(Skill equippedSkill) {
-        this.equippedSkill = equippedSkill;
-    }
-}
+}// end of class{};
