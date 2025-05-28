4f0616dd4b3b3e06a22e2deda986b23611123e8c
dhkim395 <dhkim1484@gmail.com>
2025-05-28 20:04:45 +0900

    궁수2.5차

--- src/skills/ArcherSkill.java
+++ src/skills/ArcherSkill.java
@@ -1,14 +1,18 @@
-package skills;
-
-import characters.Champion;
-
-public class ArcherSkill extends Skill {
-    public ArcherSkill(String name, int damage, int requiredMp) {
-        super(name, damage, requiredMp);
-    }
-
-    @Override
-    public Champion doSkill(Champion target) {
-        return null;
-    }
-}
+//package skills;
+//
+//import characters.Champion;
+//
+//public class WindPiercer extends Skill implements Attack{
+//  public WindPiercer {
+//      super("windPiercer",30, 20);
+//   }
+//    @Override
+//    public Champion attack(Champion target) {
+//        return null;
+//    }
+//    @Override
+//    public Champion doSkill(Champion target) {
+//        return null;
+//    }
+//
+//}
