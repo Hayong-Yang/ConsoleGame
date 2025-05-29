f4d7ab6e964efb34cfd4b5072bad3a03e7bfdd0c
kiim <kms545487@gmail.com>
2025-05-29 16:49:45 +0900

    크리티컬 데미지 구현

--- src/main/Play.java
+++ src/main/Play.java
@@ -5,15 +5,20 @@ import skills.Attack;
 import skills.FireBall;
 import skills.Skill;
 
-public class Play {
-    public static void main(String[] args) {
-        Magician magician = new Magician(100, 100, 10, 5, 0, 0, "minsu");
-        Magician hayong = new Magician(100, 100, 10, 5, 0, 0, "minsu");
+import javax.lang.model.util.SimpleTypeVisitor6;
+
+public class Play
+{
+    public static void main(String[] args)
+    {
+        Magician magician = new Magician(100, 100, 10, 5, 100, 0, "minsu");
+        Magician hayong = new Magician(100, 100, 10, 5, 0, 0, "hayong");
 
         Skill fireBall = new FireBall();
 
         fireBall.doSkill(magician, hayong);
-        hayong.toString();
+        System.out.println(hayong);
+
 
     }// main
 } //class
