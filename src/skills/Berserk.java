f9dd4dca3d1519be3906d3d1084a993c8e160d62
Hayong Yang <ssk04022@gmail.com>
2025-05-30 14:23:33 +0900

    fix3

--- src/skills/Berserk.java
+++ src/skills/Berserk.java
@@ -12,10 +12,22 @@ public class Berserk extends Skill
     @Override
     public void doSkill(Champion player, Champion target)
     {
-        target.setHp((int) (target.getHp() - (player.getPower() * 0.5) * this.getDamage()));
-        player.setMp(player.getMp() - (this.getRequiredMp() + this.getSkillLevel()));
+        boolean isCrit = player.isCritical();
+        double multiplier = isCrit ? 1.6 : 1.0;
+
+        if (isCrit) {
+            System.out.println("분노의 절규! 치명적인 광폭화 일격이 터집니다!");
+        }
+
+        int totalDamage = (int) ((player.getPower() * 0.5) * this.getDamage() * multiplier);
+        System.out.printf("데미지 %d가 들어갔습니다 효과는 굉장했다!\n", totalDamage);
+
+        target.setHp(target.getHp() - totalDamage);
+
+        player.setMp(player.getMp() - (this.getRequiredMp() + this.getSkillLevel() * 2));
+
         player.setHp(player.getHp() - 10);
         System.out.println(player.getName() + "님이 광폭화하여 HP 10을 소모했습니다!");
-    }
+    } // end of doSkill();
 }
 
--- src/skills/PowerStrike.java
+++ src/skills/PowerStrike.java
@@ -12,7 +12,18 @@ public class PowerStrike extends Skill
     @Override
     public void doSkill(Champion player, Champion target)
     {
-        target.setHp((int) (target.getHp() - (player.getPower() * 0.4) * this.getDamage()));
-        player.setMp(player.getMp() - (this.getRequiredMp() + this.getSkillLevel()));
-    }
+        boolean isCrit = player.isCritical();
+        double multiplier = isCrit ? 1.6 : 1.0;
+
+        if (isCrit) {
+            System.out.println("강철의 일격! 적의 약점을 정확히 찔렀습니다!");
+        }
+
+        int totalDamage = (int) ((player.getPower() * 0.4) * this.getDamage() * multiplier);
+        System.out.printf("데미지 %d가 들어갔습니다 효과는 굉장했다!\n", totalDamage);
+
+        target.setHp(target.getHp() - totalDamage);
+
+        player.setMp(player.getMp() - (this.getRequiredMp() + this.getSkillLevel() * 2));
+    } // end of doSkill();
 }
